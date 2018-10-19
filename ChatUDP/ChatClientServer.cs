731c258fc569ea1ca4296ac50063194b4ad29ffd
VyacheslavTretyak <tretyak1c@gmail.com>
2018-10-19 16:06:52 +0300

    finished project

---
+++ ChatUDP/ChatClientServer.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ChatUDP
+{
+	public delegate void ShowMessage(string message);
+    class ChatClientServer
+    {
+		private int localPort = 4000;
+		private int remotePort = 4000;
+		private string listernIP = "13.0.0.6";
+		private Socket listeningSocket;
+		public ShowMessage showMessage;
+		public void Start()
+		{		
+			try
+			{				
+				listeningSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+				listeningSocket.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.Broadcast, 1);
+				Task listeningTask = new Task(Listen);
+				listeningTask.Start();
+				Thread.Sleep(1000);
+				byte[] data = Encoding.Unicode.GetBytes("Start server...");
+				//EndPoint remotePoint = new IPEndPoint(IPAddress.Parse("10.10.6.6"), remotePort);
+				EndPoint remotePoint = new IPEndPoint(IPAddress.Broadcast, remotePort);
+				listeningSocket.SendTo(data, remotePoint);
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex.Message);
+			}
+			//finally
+			//{
+			//	CloseSocket();
+			//}
+		}
+
+		public void Send(string message)
+		{			
+			byte[] data = Encoding.Unicode.GetBytes(message);
+			//EndPoint remotePoint = new IPEndPoint(IPAddress.Parse("10.10.6.6"), remotePort);
+			EndPoint remotePoint = new IPEndPoint(IPAddress.Broadcast, remotePort);
+			listeningSocket.SendTo(data, remotePoint);			
+		}
+
+		
+
+		private void Listen()
+		{
+			try
+			{
+				//прослушиваем по адресу
+				IPEndPoint localIp = new IPEndPoint(IPAddress.Parse(listernIP), localPort);
+				listeningSocket.Bind(localIp);
+				while (true)
+				{
+					// полученеие сообщения 
+					StringBuilder builder = new StringBuilder();
+					int bytes = 0;
+					byte[] data = new byte[256];
+					EndPoint remoteIp = new IPEndPoint(IPAddress.Any, remotePort);
+					do
+					{
+						bytes = listeningSocket.ReceiveFrom(data, ref remoteIp);
+						builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+					} while (listeningSocket.Available > 0);
+					IPEndPoint remoteFullIp = remoteIp as IPEndPoint;		
+				
+					showMessage($"{remoteFullIp.Address}: {remoteFullIp.Port}");
+					showMessage($" >>> {builder.ToString()}");
+				}
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex.Message);
+			}
+			finally
+			{
+				CloseSocket();
+			}
+		}
+
+		private void CloseSocket()
+		{
+			listeningSocket?.Shutdown(SocketShutdown.Both);
+			listeningSocket?.Close();
+			listeningSocket = null;
+		}
+	}
+}
--- ChatUDP/MainWindow.xaml.cs
+++ ChatUDP/MainWindow.xaml.cs
@@ -20,9 +20,90 @@ namespace ChatUDP
 	/// </summary>
 	public partial class MainWindow : Window
 	{
+		Color color = Color.FromRgb(255, 191, 128);
+		ChatClientServer chat = new ChatClientServer();
 		public MainWindow()
 		{
 			InitializeComponent();
+
+				
+			TextBoxLogin.GotFocus += TextBoxLogin_GotFocus;
+			TextBoxLogin.LostFocus += TextBoxLogin_LostFocus;
+			TextBoxLogin.TextChanged += TextBoxLogin_TextChanged;
+			TextBoxMessage.GotFocus += TextBoxMessage_GotFocus;
+			TextBoxMessage.LostFocus += TextBoxMessage_LostFocus;
+			TextBoxMessage.TextChanged += TextBoxMessage_TextChanged;
+			TextBoxLogin.Foreground = new SolidColorBrush(color);
+			TextBoxLogin.Text = "Login";
+			TextBoxMessage.Foreground = new SolidColorBrush(color);
+			TextBoxMessage.Text = "Message";
+			ButtonSend.Click += ButtonSend_Click;
+			
+
+			chat.showMessage = ShowMessage;
+			chat.Start();
+		}
+
+		private void ButtonSend_Click(object sender, RoutedEventArgs e)
+		{
+			chat.Send($"{TextBoxLogin.Text} : {DateTime.Now.ToString()} \n {TextBoxMessage.Text}");
+			TextBoxMessage.Text = "";
+		}
+
+		private void ShowMessage(string message)
+		{
+			Dispatcher.Invoke(() =>
+			{
+				ListBoxMessages.Items.Add(message);
+				ListBoxMessages.ScrollIntoView(ListBoxMessages.Items.GetItemAt(ListBoxMessages.Items.Count - 1));
+			});
+			
+		}
+
+		private void TextBoxLogin_TextChanged(object sender, TextChangedEventArgs e)
+		{
+			ButtonSend.IsEnabled = !(TextBoxLogin.Text == "Login" || TextBoxLogin.Text == "");
+		}
+
+		private void TextBoxMessage_TextChanged(object sender, TextChangedEventArgs e)
+		{
+			ButtonSend.IsEnabled = !(TextBoxMessage.Text == "Message" || TextBoxMessage.Text == "");
+		}
+
+		private void TextBoxMessage_LostFocus(object sender, RoutedEventArgs e)
+		{
+			RemoveFocus(sender, "Message");
+		}
+		private void TextBoxMessage_GotFocus(object sender, RoutedEventArgs e)
+		{
+			SetFocus(sender, "Message");
+		}
+		private void TextBoxLogin_LostFocus(object sender, RoutedEventArgs e)
+		{
+			RemoveFocus(sender, "Login");
+		}
+		private void TextBoxLogin_GotFocus(object sender, RoutedEventArgs e)
+		{
+			SetFocus(sender, "Login");
+		}
+		private void RemoveFocus(object sender, string text)
+		{
+			TextBox tb = sender as TextBox;
+			if (tb.Text == "")
+			{
+				tb.Foreground = new SolidColorBrush(color);
+				tb.Text = text;
+			}
+		}
+		private void SetFocus(object sender, string text)
+		{
+			TextBox tb = sender as TextBox;
+			if (tb.Text == text)
+			{
+				tb.Foreground = new SolidColorBrush(Colors.Black);
+				tb.Text = "";
+			}
 		}
 	}
 }
+
